c9c38fee2721520df0b5eca6c06e7b44431d6b83
AnjaMaksimovic <maksimovicanja11@gmail.com>
2023-11-18 15:05:41 +0100

    Registration and login frontend implemented.

--- isa-frontend/src/app/app.module.ts
+++ isa-frontend/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LayoutModule } from './feature-modules/layout/layout.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './infrastructure/auth/auth.module';
+import { MaterialModule } from './infrastructure/material/material.module';
+import { JwtInterceptor } from './infrastructure/auth/jwt/jwt.interceptor';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    AuthModule,
+    HttpClientModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- isa-frontend/src/app/shared/shared.module.ts
+++ isa-frontend/src/app/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthModule } from '../infrastructure/auth/auth.module';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
